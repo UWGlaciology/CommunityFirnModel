fixed issue in regrid: Tz is now a weighted average; includes r2

firnmodel/CFM_main/firn_density_spin.py
@@ -331,7 +331,7 @@ class FirnDensitySpin:
 			if self.doublegrid:
 				self.gridtrack = np.concatenate(([1],self.gridtrack[:-1]))
 				if self.gridtrack[-1]==2:
-					self.dz, self.z, self.rho, self.Tz, self.mass, self.sigma, self. mass_sum, self.age, self.bdot_mean, self.LWC, self.gridtrack = regrid(self)
+					self.dz, self.z, self.rho, self.Tz, self.mass, self.sigma, self. mass_sum, self.age, self.bdot_mean, self.LWC, self.gridtrack, self.r2 = regrid(self)
 
 			# write results at the end of the time evolution
 			if (iii == (self.stp - 1)):

firnmodel/CFM_main/regrid.py
@@ -20,7 +20,7 @@ def regrid(self):
 	g2dz 	= np.array([np.sum(self.dz[ind1])])
 	g2mass 	= np.sum(self.mass[ind1])
 	g2rho 	= g2mass/g2dz
-	g2Tz 	= np.array([np.mean(self.Tz[ind1])])
+	g2Tz 	= np.sum(self.Tz[ind1]*self.mass[ind1])/g2mass # Use a weighted average for temperature (effectively the enthalpy)
 	g2gt 	= 2 #gridtrack
 	g2age 	= np.mean(self.age[ind1])
 	g2bm 	= np.mean(self.bdot_mean[ind1])
@@ -51,7 +51,12 @@ def regrid(self):
 	self.LWC 		= np.concatenate((self.LWC[0:ind1a],[g2lwc],self.LWC[ind1b+1:-1],g3lwc))
 	self.gridtrack 	= np.concatenate((self.gridtrack[0:ind1a],[g2gt],self.gridtrack[ind1b+1:-1],g3gt))
 
-	return self.dz, self.z, self.rho, self.Tz, self.mass, self.sigma, self. mass_sum, self.age, self.bdot_mean, self.LWC, self.gridtrack
+	if self.c['physGrain']:
+		g2r2 	= np.mean(self.r2)
+		g3r2 	= self.r2[-1]* np.ones(self.nodestocombine)
+		self.r2 = np.concatenate((self.r2[0:ind1a],g2r2,self.r2[ind1b+1:-1],g3r2)) 
+
+	return self.dz, self.z, self.rho, self.Tz, self.mass, self.sigma, self.mass_sum, self.age, self.bdot_mean, self.LWC, self.gridtrack, self.r2
 
 def init_regrid(self):
 	'''