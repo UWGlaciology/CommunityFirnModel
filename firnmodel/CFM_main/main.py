739c2862b8e480657cb04a791837a05e63967a23
maximusjstevens <maximusjstevens@gmail.com>
2019-02-08 16:36:24 -0800

    update version in main.py

--- firnmodel/CFM_main/main.py
+++ firnmodel/CFM_main/main.py
@@ -10,7 +10,7 @@ import json
 
 __author__ = "C. Max Stevens, Vincent Verjans, Brita Horlings, Annikah Horlings, Jessica Lundin"
 __license__ = "MIT"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __maintainer__ = "Max Stevens"
 __email__ = "maxstev@uw.edu"
 __status__ = "Production"
