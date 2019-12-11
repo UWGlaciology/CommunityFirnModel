977fa0fb8c8348185b6510c3f271eb7c863def06
maximusjstevens <maximusjstevens@gmail.com>
2019-12-11 13:56:40 -0800

    updated plotter

--- CFM_main/plotter.py
+++ CFM_main/plotter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+'''
+Example script to load and plot results from the CFM.
+'''
 
 # Copyright (C) 2019 C. Max Stevens <maxstev@uw.edu>
-#
 # Distributed under terms of the MIT license.
 
 import matplotlib.pyplot as plt 
@@ -14,26 +16,32 @@ import pickle
 import seaborn as sns 
 sns.set()
 
-'''
-Example of plotting results from the CFM.
-'''
+def plotter(rfolder,saver):
+	'''
+	Function that plots.
+	'''
 
-rfolder = '/PATH/TO/RESULTS/FOLDER' # alter this to point to the results folder.
-rfile = 'CFMresults.hdf5'
-fn = os.path.join(rfolder,rfile)
-f = h5.File(fn,'r')
+	rfile = 'CFMresults.hdf5'
+	fn = os.path.join(rfolder,rfile)
+	f = h5.File(fn,'r')
 
-timesteps = f['depth'][1:,0]
-stps = len(timesteps)
-depth = f['depth'][1:,1:]
-density = f['density'][1:,1:]
-temperature = f['temperature'][1:,1:]
-dip_all = f['DIP'][:,:]
-f.close()
+	timesteps = f['depth'][1:,0]
+	stps = len(timesteps)
+	depth = f['depth'][1:,1:]
+	density = f['density'][1:,1:]
+	temperature = f['temperature'][1:,1:]
+	dip_all = f['DIP'][:,:]
+	f.close()
 
-f1,a1 = plt.subplots()
-a1.plot(density[-1,:],depth[-1,:])
-a1.invert_yaxis()
-a1.grid(True)
-f1.savefig('Example_DepthDensity.png')
+	f1,a1 = plt.subplots()
+	a1.plot(density[-1,:],depth[-1,:])
+	a1.invert_yaxis()
+	a1.grid(True)
+	if saver:
+		f1.savefig('Example_DepthDensity.png')
+
+if __name__ == '__main__':
+	
+	saver = True
+	rfolder = '/PATH/TO/RESULTS/FOLDER' # alter this to point to the results folder.
 
