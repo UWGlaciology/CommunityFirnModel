94312a5371e101fb131d927184005cae2143c89e
oraschewski <36086882+oraschewski@users.noreply.github.com>
2022-05-17 08:58:28 +0200

    Revert "Delete __init__.py"
    This reverts commit 014a60fe75e4541607765a71270e390912a14036.

---
+++ CFM_main/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2019 Max Stevens <maxstev@uw.edu>
+#
+# Distributed under terms of the MIT license.
+
+'''
+The Community Firn Model
+========================
+
+Something here.
+'''
